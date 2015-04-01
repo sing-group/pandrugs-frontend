91b99e74f4dc426a0e2883834d78b83d914cecd4
lipido <dgpena@uvigo.es>
2015-04-01 17:12:22 +0200

    Add more data to the mocked database

--- app/scripts/services/mockdatabase.js
+++ app/scripts/services/mockdatabase.js
@@ -8,7 +8,7 @@
  * Factory in the pandrugsdbFrontendApp.
  */
 angular.module('pandrugsdbFrontendApp')
-  .factory('MockDatabase', ['$q', function mockDatabaseFactory($q) {
+  .factory('MockDatabase', ['$q', '$timeout', function mockDatabaseFactory($q, $timeout) {
     // Service logic
     // ...
 
@@ -18,13 +18,87 @@ angular.module('pandrugsdbFrontendApp')
 	
 	var deferred = $q.defer();
 	
-	deferred.resolve(
-	  [
-	    { gene: 'BRCA2' }
-	  ]
-	);
+	$timeout(function() {
+	    deferred.resolve(
+	      [
+		{ 
+		  gene: 'BRCA2',
+		  drug: 'AFATINIB',
+		  family: 'Other',
+		  source: 'GDSC',
+		  status: 'Approved',
+		  cancer: 'metastatic non-small cell lung cancer with EGFR mutations',
+		  therapy: 'TARGETED THERAPY',
+		  indirect: '-',
+		  target: '',
+		  score: 0.5
+		},
+		{ 
+		  gene: 'BRCA2',
+		  drug: 'IDARUBICIN',
+		  family: 'Other',
+		  source: 'CTRP',
+		  status: 'Approved',
+		  cancer: 'acute myelogenous leukemia',
+		  therapy: 'CHEMOTHERAPY',
+		  indirect: '-',
+		  target: '',
+		  score: 0.5
+		},
+		{ 
+		  gene: 'BRCA2',
+		  drug: 'PEMETREXED',
+		  family: 'Other',
+		  source: 'CTRP',
+		  status: 'Approved',
+		  cancer: 'malignant pleural mesothelioma',
+		  therapy: 'CHEMOTHERAPY',
+		  indirect: '-',
+		  target: '',
+		  score: 0.5
+		},
+		{ 
+		  gene: 'BRCA2',
+		  drug: 'ABT-751',
+		  family: 'Other',
+		  source: 'CTRP',
+		  status: 'Clinical',
+		  cancer: 'cancer',
+		  therapy: '',
+		  indirect: '-',
+		  target: '',
+		  score: 0.3
+		},
+		{ 
+		  gene: 'BRCA2',
+		  drug: 'BMN673',
+		  family: 'Other',
+		  source: 'ClearityFoundationBiomarkers',
+		  status: 'Clinical',
+		  cancer: 'cancer',
+		  therapy: '',
+		  indirect: '-',
+		  target: '',
+		  score: 0.3
+		}
+		
+	      ]
+	    )	  
+	}, 1000);
 	
         return deferred.promise;
       }
     };
   }]);
+/*
+ * <td>{{row.gene}}</td>
+		<td>{{row.drug}}</td>
+		<td>{{row.family}}</td>
+		<td>{{row.source}}</td>
+		<td>{{row.status}}</td>
+		<td>{{row.cancer}}</td>
+		<td>{{row.therapy}}</td>
+		<td>{{row.indirect}}</td>
+		<td>{{row.target}}</td>
+		<td>{{row.score}}</td>
+		*/
