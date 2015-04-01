73a8c2aa25485805c018c27ea739ee934b85ed41
lipido <dgpena@uvigo.es>
2015-04-01 17:13:24 +0200

    Add more tests

--- test/spec/controllers/query.js
+++ test/spec/controllers/query.js
@@ -5,18 +5,49 @@ describe('Controller: QueryCtrl', function () {
   // load the controller's module
   beforeEach(module('pandrugsdbFrontendApp'));
 
-  var QueryCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    QueryCtrl = $controller('QueryCtrl', {
-      $scope: scope
-    });
+  var $controller;
+  var $q;
+  var $scope;
+  
+  beforeEach(inject(function (_$controller_, _$q_, _$rootScope_) {
+    $controller = _$controller_;
+    $q = _$q_;
+    $scope = _$rootScope_.$new();
   }));
 
-  /*it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });*/
+  describe('$scope.query', function() {
+    var service;    
+    var deferred;
+    
+    beforeEach(function() {
+      service = {search: function() {}};      
+      $controller('QueryCtrl', { $scope: $scope, MockDatabase: service });
+      deferred = $q.defer();      
+      spyOn(service, 'search').and.returnValue(deferred.promise);
+    });
+    
+    it('should call search function on the service', function() {
+      $scope.genes='BRCA2';
+      var tableState = null;
+      
+      $scope.query(tableState);
+      
+      deferred.resolve([]);
+      $scope.$digest(); // force then method in promise to run
+      
+      expect(service.search).toHaveBeenCalledWith('BRCA2', tableState);      
+    });
+    
+    it('should put results on $scope.results', function() {
+      $scope.genes='BRCA2';
+      var tableState = null;
+      
+      $scope.query(tableState);
+      
+      deferred.resolve([{gene:'BRCA2'}, {gene:'BRCA2'}]);
+      $scope.$digest(); // force then method in promise to run
+      
+      expect($scope.results.length).toBe(2);
+    });
+  });
 });
