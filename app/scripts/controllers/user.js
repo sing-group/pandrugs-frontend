9ecd53c0a3e034a03c2662074d5bb73035435da9
Miguel Reboiro-Jato <mrjato@uvigo.es>
2017-05-08 13:25:53 +0200

    Adds help texts and social links
    Adds several help texts and other clarifications to several parts of the
    application. This includes a reworking of the login page.
    
    In addition, several social links have been added to the footer and some URLs
    have been updated.

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -10,48 +10,60 @@
 angular.module('pandrugsFrontendApp')
   .controller('UserCtrl', ['user', '$scope', '$location',
     function (user, $scope, $location) {
-    if ($location.search().confirmuuid) {
-          user.confirm($location.search().confirmuuid, function(){
-            window.alert('Your user has been successfully activated. You can now login.');
-            $location.path('/login');
-          }, function(){
-            window.alert('ERROR: Your user could not be activated.');
-          });
+      if ($location.search().confirmuuid) {
+            user.confirm($location.search().confirmuuid, function(){
+              window.alert('Your user has been successfully activated. You can now login.');
+              $location.path('/login');
+            }, function(){
+              window.alert('ERROR: Your user could not be activated.');
+            });
+      }
+
+      $scope.newUser = false;
+
+      $scope.showRegistration = function () {
+        console.log('registration');
+        $scope.newUser = true;
+      };
+
+      $scope.showLogin = function () {
+        console.log('login');
+        $scope.newUser = false;
+      };
+
+      $scope.doLogin = function() {
+        user.login(
+          $scope.login,
+          $scope.password,
+          function() { $location.path('/'); },
+          function() { window.alert('Your login/password was not correct. Please make sure that you have been registered with this login/password'); }
+        );
+      };
+
+      $scope.register = function() {
+        user.register(
+          $scope.newlogin,
+          $scope.newemail,
+          $scope.newpassword,
+          function() {
+            window.alert('Your user has been now register. We have sent you an email in order to activate it. Please check your inbox');
+            $scope.newlogin = '';
+            $scope.newemail = '';
+            $scope.newpassword = '';
+          },
+          function() {
+            window.alert('An error has occurred during registration. Please contact administrator');
+          }
+        );
+      };
+
+      $scope.currentUser = function() {
+        return user.getCurrentUser();
+      };
+
+      $scope.logout = function() {
+        user.logout();
+      };
+
     }
-
-
-    $scope.doLogin = function() {
-      user.login(
-        $scope.login,
-        $scope.password,
-        function() { $location.path('/'); },
-        function() { window.alert('Your login/password was not correct. Please make sure that you have been registered with this login/password'); }
-      );
-    };
-
-    $scope.register = function() {
-      user.register(
-        $scope.newlogin,
-        $scope.newemail,
-        $scope.newpassword,
-        function() {
-          window.alert('Your user has been now register. We have sent you an email in order to activate it. Please check your inbox');
-          $scope.newlogin = '';
-          $scope.newemail = '';
-          $scope.newpassword = '';
-        },
-        function() {
-          window.alert('An error has occurred during registration. Please contact administrator');
-        }
-      );
-    };
-
-    $scope.currentUser = function() {
-      return user.getCurrentUser();
-    };
-
-    $scope.logout = function() {
-      user.logout();
-    };
-
-  }]);
+  ]);
