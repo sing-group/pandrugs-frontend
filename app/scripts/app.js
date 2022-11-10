10e9293f0840868ca3329d2ef55bbc72236de79f
lipido <dgpena@uvigo.es>
2022-11-10 02:33:57 +0100

    Sets backend url to the online backend

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,11 +46,11 @@ angular
   .constant('BACKEND', (function() {
     // Service logic
     // ...
-    //var server = 'https://www.pandrugs.org';
+    var server = 'https://www.pandrugs.org';
     //var server = 'http://sing.ei.uvigo.es';
     //var server = 'http://mrjato.sing-group.org:8080'; // development: test server
     //var server = 'http://0.0.0.0:9000'; // development: via grunt reverse proxy to local backend
-    var server = ''; // the server is in this machine (e.g.: docker distribution)
+    //var server = ''; // the server is in this machine (e.g.: docker distribution)
     var app = server + '/pandrugs-backend/';
 
 
