2b039455bf8166153e8b62a63596e6b4dd2393e4
Alba Nogueira Rodríguez <alnogueira@uvigo.es>
2022-12-16 18:01:02 +0100

    Includes 'Bower install' changes after including angular-list-group lib

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-list-group/dist/angular-list-group.min.js',
+      'bower_components/angular-list-group/dist/angular-list-group.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-touch/angular-touch.js',
